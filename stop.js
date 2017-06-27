3713dce5ff8b5846b146e8655672dae696fe52a2
Forbes Lindesay <forbes@lindesay.co.uk>
2017-06-27 17:27:21 +0100

    Deployment

--- stop.js
+++ stop.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var url = require('url');
+var fs = require('fs');
 var stop = require('stop');
+var s3 = require('s3');
 
 var server = require('./server.js');
 
@@ -11,17 +13,40 @@ stop.getWebsiteStream('http://localhost:3000', {
   },
   parallel: 1
 })
-.syphon(stop.addFavicon())
-.syphon(stop.addManifest('/app.manifest', {addLinks: true}))
-.on('data', function (page) {
-  if (page.statusCode !== 200) {
-    throw new Error('Unexpected status code ' + page.statusCode +
-                    ' for ' + page.url);
-  }
-  console.log(page.statusCode + ' - ' + page.url);
-})
+//.syphon(stop.addFavicon())
+.syphon(stop.minifyJS())
+.syphon(stop.minifyCSS({deadCode: true, silent: true}))
+.syphon(stop.log())
+.syphon(stop.checkStatusCodes([200]))
 .syphon(stop.writeFileSystem(__dirname + '/out'))
 .wait().done(function () {
   server.close();
-  console.log('success');
+  console.log('done building website');
+  if (
+    process.env.TRAVIS_BRANCH !== 'master' ||
+    process.env.TRAVIS_PULL_REQUEST !== 'false'
+  ) {
+    return;
+  }
+  var client = s3.createClient({
+    s3Options: {
+      accessKeyId: process.env.S3_KEY,
+      secretAccessKey: process.env.S3_SECRET,
+      region: process.env.S3_REGION
+    },
+  });
+  var uploader = client.uploadDir({
+    localDir: __dirname + '/out',
+    deleteRemoved: true,
+    s3Params: {
+      Bucket: process.env.S3_BUCKET,
+      Prefix: ''
+    }
+  });
+  uploader.on('error', function(err) {
+    console.error('unable to sync:', err.stack);
+  });
+  uploader.on('end', function() {
+    console.log("done uploading website");
+  });
 });
